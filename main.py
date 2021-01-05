9334e8d3ecf81424760ff672b42fd3eb2aa411c1
Justin Li <nitsujil3002@gmail.com>
2021-01-05 11:37:09 -0800

    Implemeted quit function

--- main.py
+++ main.py
@@ -21,24 +21,45 @@ WE CAN'T LET THEM HAVE IT
 ONE GOAL: SURVIVAL... and THE CAR
 REACH THE END BEFORE THE MAN GONNA GETCHU"""
 
-def main():
-    pass
-
-    # Display introduction
+CHOICES = """
+    ----
+    Q. QUIT
+    ----
+"""
+def intro():
     for char in textwrap.dedent(INTRODUCTION):
         time.sleep(0.05)
         sys.stdout.write(char)
         sys.stdout.flush()
 
     time.sleep(1)
+
+
+def main():
+    intro()
+
+    # Variables
+    done = False
+
+
     # MAIN LOOP
+    while not done:
         # TODO: Check to see if reached END GAME
 
         # TODO: Present the users their choices
+        print(CHOICES)
+
+        user_choice = input("What do you want to do?\n").lower().strip(",.!?")
+
+        if user_choice == "q":
+            done = True
 
         # TODO: Change the environment based on user choice and RNG
 
         # TODO: Random event generator
+
+    # Outro
+    print("Thanks for playing. Play again soon!")
 
 if __name__ == "__main__":
     main()
