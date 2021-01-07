455c1b096b3975e3ad692b64788b881be89c9e9a
Justin Li <nitsujil3002@gmail.com>
2021-01-07 11:42:17 -0800

    implemented hunger and ninja

--- main.py
+++ main.py
@@ -22,8 +22,33 @@ WE CAN'T LET THEM HAVE IT
 ONE GOAL: SURVIVAL... and THE CAR
 REACH THE END BEFORE THE MAN GONNA GETCHU"""
 
+WIN = """
+YOU PRESS THE BUTTON TO OPEN THE GATE. 
+THIS ISN'T THE FIRST TIME YOU'VE DONE THIS
+YOU CAN TIME IT PERFECTLY SO THAT YOU CAN 
+SLIDE THE CAR IN AS THE GATES CLOSE.
+
+YOU KNOW YOU DID THE RIGHT THING.
+THE GOVERNMENT WOULD HAVE TORN THE CAR APART
+ANALYSING IT, TESTING IT, THEN DESTROYING IT.
+
+THEY DON'T KNOW ITS SECRETS...
+THAT IT HOLDS THE KEY TO DIFFERENT WORLDS.
+
+AS YOU STEP OUT THE VEHICLE, FIDO RUNS 
+UP TO YOU.
+"THANK YOU FOR SAVING ME," HE SAYS.
+
+AS YOU TAKE A COUPLE OF STEPS AWAY FROM THE CAR, 
+IT MAKES A STRANGE NOISE
+
+BEFORE YOUR EYES, IT SHIFTS ITS SHAPE YOU'VE SEEN IT BEFORE, BUT ONLY ON TV.
+
+BUMBLEBEE...?"""
+
 CHOICES = """
     ----
+    A. DRINK A CHUG JUG
     B. GO AT A MODERATE SPEED
     C. GO FULL THROTTLE
     D. GAS STATION
@@ -31,8 +56,10 @@ CHOICES = """
     Q. QUIT
     ----
 """
-def intro():
-    for char in textwrap.dedent(INTRODUCTION):
+
+
+def type_text_output(string):
+    for char in textwrap.dedent(string):
         time.sleep(0.05)
         sys.stdout.write(char)
         sys.stdout.flush()
@@ -41,36 +68,69 @@ def intro():
 
 
 def main():
-    # intro()
-
     # CONSTANTS
     MAX_FUEL_LEVEL = 50
+    MAX_DISTANCE_TRAVELED = 100
+    MAX_CHUG_JUG = 3
 
     # Variables
     done = False
 
-    kms_traveled = 0       # 100km is the end
-    agent_distance = -20    # 0km is the end
+    kms_traveled = 0
+    agent_distance = -20
     turns = 0
-    tofu = 3                # 3 is max
+    chug_jug = MAX_CHUG_JUG
     fuel = MAX_FUEL_LEVEL
-    hunger = 0
+    thirst = 0
 
     # MAIN LOOP
     while not done:
+        # TODO: RNG
+        # NINJA - refills your food (5%)
+        if chug_jug < 3 and random.random() < 0.05:
+            # Refill chug_jugs
+            chug_jug = MAX_CHUG_JUG
+            # Player feedback
+            print("******** You look at your Chug Jug container.")
+            print("******** It is filled magically")
+            print("******** \"You're welcome kid.\"")
+            print("******** You look around. Ninja?")
+
         # TODO: Check to see if reached END GAME
+        # WIN - Traveled the distance required
+        if kms_traveled > MAX_DISTANCE_TRAVELED:
+            # Print win scenario
+            time.sleep(2)
+            type_text_output(WIN)
+            # Break
+            break
 
         # TODO: Present the users their choices
         print(CHOICES)
 
         user_choice = input("What do you want to do?\n").lower().strip(",.!?")
-        if user_choice == "b":
+        if user_choice == "a":
+            # Drink/Energy
+            if chug_jug > 0:
+                chug_jug -= 1
+                thirst = 0
+                print()
+                print("-------- That's refreshing.")
+                print("-------- Your thirst is sated.")
+                print()
+
+            else:
+                print()
+                print("You're out of Chug Jugs.")
+                print()
+
+        elif user_choice == "b":
             # Moderate Speed
-            player_distance_now = random.randrange(8, 13)
-            agent_distance_now = random.randrange(6, 11)
+            player_distance_now = random.randrange(7, 13)
+            agent_distance_now = random.randrange(7, 13)
 
             # Burn fuel
-            fuel -= random.randrange(3, 8)
+            fuel -= random.randrange(2, 7)
 
             # Player distance traveled
             kms_traveled += player_distance_now
@@ -80,8 +140,8 @@ def main():
 
             # Player feedback
             print()
-            print("Zoooooom!")
-            print(f"-------- You travelled {player_distance_now} kms.")
+            print("--------- Zoooooom!")
+            print(f"-------- You traveled {player_distance_now} kms.")
             print()
 
         elif user_choice == "c":
@@ -100,7 +160,7 @@ def main():
 
             # Player feedback
             print()
-            print("ZOOOOOOOM!")
+            print("-------- ZOOOOOOOM!")
             print(f"-------- You travelled {player_distance_now} kms.")
             print()
 
@@ -123,20 +183,25 @@ def main():
             print(f"\tkm travelled: {kms_traveled}.")
             print(f"\tFuel remaining: {fuel} L.")
             print(f"\tAgents are now {abs(agent_distance)} kms behind.")
-            print(f"\tYou have {tofu} tofus left.")
+            print(f"\tYou have {chug_jug} Chug Jugs left.")
+            print(f"\tThirst: {thirst}.")
             print(f"\t--------\n")
 
         elif user_choice == "q":
             done = True
 
+        # Hunger
+        if user_choice not in ["a", "e"]:
+            thirst += random.randrange(5, 13)
+
         time.sleep(1.5)
 
-        # TODO: Change the environment based on user choice and RNG
 
-        # TODO: Random event generator
+
 
     # Outro
     print("Thanks for playing. Play again soon!")
+
 
 if __name__ == "__main__":
     main()
